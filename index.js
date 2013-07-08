d55f49d8b5bc553b828cb20022baa5ac671e1187
Michael Wittig <michael.wittig@tullius-walden.com>
2013-07-08 08:33:53 +0200

    refactoring

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-var safejson = require("./lib/safejson");
+var events = require("events"),
+	util = require("util"),
+	assert = require("assert-plus"),
+	safejson = require("./lib/safejson");
+
+function Endpoint(debug, info, error, critical, name) {
+	assert.bool(debug, "debug");
+	assert.bool(info, "info");
+	assert.bool(error, "error");
+	assert.bool(critical, "critical");
+	assert.string(name, "name");
+	events.EventEmitter.call(this);
+	this.levels = {
+		debug: debug,
+		info: info,
+		error: error,
+		critical: critical
+	};
+	this.name = name;
+	this.logErrCount = 0;
+	this.stopping = false;
+	this.stopped = false;
+}
+util.inherits(Endpoint, events.EventEmitter);
+Endpoint.prototype.stop = function(callback) {
+	this.stopping = true;
+	var self = this;
+	this._stop(function(err) {
+		if (err) {
+			callback(err);
+		} else {
+			self.stopped = true;
+			try {
+				callback();
+			} finally {
+				self.emit("stop");
+			}
+		}
+	});
+};
+Endpoint.prototype.log = function(log, callback) {
+	this._log(log, callback);
+};
 
 exports.safejson = safejson;
+exports.Endpoint = Endpoint;
---
+++ test/endpoint.js
@@ -0,0 +1,29 @@
+var assert = require("assert-plus"),
+	util = require("util"),
+	index = require("../index");
+
+describe("API", function() {
+	describe("Endpoint", function() {
+		it("should work if all params are set", function(){
+			new index.Endpoint(true, true, true, true, "test");
+		});
+		it("should not work if one of params are not set", function(){
+			assert.throws(function() {
+				new index.Endpoint(true, true, true, true);
+			});
+		});
+		it("should not work if one of params is not not bool", function(){
+			assert.throws(function() {
+				new index.Endpoint(true, true, true, "true", "test");
+			});
+		});
+		it("should emit events", function(done){
+			var endpoint = new index.Endpoint(true, true, true, true, "test");
+			endpoint.once("test", function(err) {
+				if (err) throw err;
+				done();
+			});
+			endpoint.emit("test");
+		});
+	});
+});
